efcbc18dba31e26af24c397d2f9c3d7166d4665b
MarcelCookie <prmcltvr0@outlook.com>
2023-06-19 16:21:38 +0200

    Compare choices and declare winner for one round

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -1,10 +1,29 @@
-function GetComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+
+//Generate a random choice for the computer
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   }
-  const ComputerSelection = GetComputerChoice();
-  console.log(ComputerSelection);
+  const computerSelection = getComputerChoice();
+  console.log(computerSelection);
 
-const playerSelection = prompt("Choose between 'rock', 'paper', or 'scissors'");
+//Ask the player to choose between "rock" OR "paper" OR "scissors"
+const playerSelection = prompt("Choose between 'rock', 'paper', or 'scissors'","scissors");
 console.log(playerSelection);
+
+//Compare the choices AND declare the winner for a single round
+function playRound(playerSelection, computerSelection) {
+  switch (playerSelection.toLowerCase() + computerSelection.toLowerCase()) {
+    case "rockscissors":
+    case "paperrock":
+    case "scissorspaper":
+      return "You Win!";
+    case "rockpaper":
+    case "paperscissors":
+    case "scissorsrock":
+      return "You Loose!";
+    default:
+      return "No winner this time";
+  }
+}console.log(playRound())
